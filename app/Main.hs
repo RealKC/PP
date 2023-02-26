c8986490a97adbbfee2bc75125831a6cb6c731ed
Mițca Dumitru <dumitru0mitca@gmail.com>
2023-02-26 18:57:06 +0200

    Add punctuation removal step

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,13 @@ module Main where
 
 import System.IO
 
+removePunctuation :: String -> String
+removePunctuation = filter (`notElem` ".,?!-_:;/()'\"")
+
 main :: IO ()
 main = do
   putStr "Introduce numele fisierului pentru a fi procesat: "
   hFlush stdout
   filePath <- getLine
   contents <- readFile filePath
-  putStr contents
+  putStr (removePunctuation contents)
