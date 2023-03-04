7bca1cc3a542e2682a5fb7a19635cd07c56d0e11
Mițca Dumitru <dumitru0mitca@gmail.com>
2023-03-04 23:58:39 +0200

    Tema2: Adauga rezolvarea exercitiului 2

---
+++ Tema2/Exercitiul2/main.py
@@ -0,0 +1,34 @@
+import polyglot
+import subprocess
+
+
+def r_main(output, colour):
+    lin_reg = polyglot.eval(language="R", string="""
+main <- function(output, colour) {
+    library("lattice")
+
+    data <- read.csv('dataset.txt')
+    linReg <- lm(x ~ y, data)
+
+    input <- file("")
+
+    svg(output)
+    plot(linReg, col = colour)
+}
+""")
+    lin_reg(output, colour)
+
+
+def main():
+    output_name = input('Name of file to output to: ')
+    output_path = input('Path to output to: ')
+    colour = input('Input the colour for the points: ')
+
+    output = output_path + '/' + output_name
+    r_main(output, colour)
+
+    subprocess.run(['xdg-open', output])
+
+
+if __name__ == '__main__':
+    main()
