f1c8f2f5f57949c09b22b9350a1b29233fc474dd
Mițca Dumitru <dumitru0mitca@gmail.com>
2023-03-27 23:35:51 +0300

    Tema5/Exercitiul2: Permite unui jucator sa castige

--- Tema5/Exercitiul2/main.py
+++ Tema5/Exercitiul2/main.py
@@ -3,8 +3,8 @@ from pygame.locals import *
 
 
 class App:
-    X = 1
-    O = 2
+    X = 'X'
+    O = 'O'
 
     def __init__(self):
         self._running = True
@@ -16,7 +16,6 @@ class App:
             Rect(20, 230, 100, 100), Rect(120, 230, 100, 100), Rect(220, 230, 100, 100)
         ]
         self._game_matrix = [0] * 9
-        print(type(self._game_matrix))
         self._current_player = self.X
 
     def _toggle_player(self):
@@ -25,9 +24,36 @@ class App:
         else:
             self._current_player = self.X
 
+    def _winner(self):
+        gm = self._game_matrix
+
+        if (gm[0] == gm[4] == gm[8] or gm[2] == gm[4] == gm[6]) and gm[4] != 0:
+            return f'{gm[4]} has won!'
+
+        if gm[0] == gm[1] == gm[2] and gm[0] != 0:
+            return f'{gm[0]} has won!'
+
+        if gm[3] == gm[4] == gm[5] and gm[3] != 0:
+            return f'{gm[3]} has won!'
+
+        if gm[6] == gm[7] == gm[8] and gm[6] != 0:
+            return f'{gm[6]} has won!'
+
+        if gm[0] == gm[3] == gm[6] and gm[0] != 0:
+            return f'{gm[0]} has won!'
+
+        if gm[1] == gm[4] == gm[6] and gm[1] != 0:
+            return f'{gm[1]} has won!'
+
+        if gm[2] == gm[5] == gm[8] and gm[8] != 0:
+            return f'{gm[8]} has won!'
+
+        return None
+
     def on_init(self):
         pygame.init()
         self._display_surface = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._font = pygame.font.SysFont("Fira Code", 20)
         self._running = True
 
     def on_event(self, event):
@@ -55,6 +81,12 @@ class App:
                 case self.O:
                     pygame.draw.circle(self._display_surface, "blue", (square.x + square.w / 2, square.y + square.h / 2), square.h / 2)
                     pygame.draw.circle(self._display_surface, "white", (square.x + square.w / 2, square.y + square.h / 2), square.h / 2 - 2)
+
+        winner = self._winner()
+        if winner is not None:
+            text = self._font.render(winner, True, "black")
+            self._display_surface.blit(text, (330, 100))
+
         pygame.display.flip()
 
     def on_cleanup(self):
