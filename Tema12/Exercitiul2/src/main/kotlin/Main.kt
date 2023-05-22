7e12ca1e65a94c7349263b8614be135cef1ec320
Mițca Dumitru <dumitru0mitca@gmail.com>
2023-05-22 22:31:49 +0300

    Tema12: Adauga rezolvarea exercitiului 2

---
+++ Tema12/Exercitiul2/src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+import java.io.File
+import java.util.Scanner
+
+fun rotn(s: String, n: Int) = s.map {
+    val x = it + 13
+    val limit = if (it.isUpperCase()) 'Z' else 'z'
+    when {
+        it.isLetter() -> if (x > limit) x - 26 else x
+        else -> it
+    } }.fold("") { acc, ch -> acc + ch }
+
+fun main(args: Array<String>) {
+    print("Input the offset to rotate words by: ")
+    val n = Scanner(System.`in`).nextInt()
+
+    val file = File("loremipsum.txt")
+    file.forEachLine { line ->
+        line.split(" ")
+            .map { if (it.length in 4..7) rotn(it, n) else it }
+            .forEach { print("$it ") }
+    }
+}
