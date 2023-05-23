e7ebaa6a460ac51a2e49b1b3ba7df81d1a31297c
Mițca Dumitru <dumitru0mitca@gmail.com>
2023-05-23 08:22:17 +0300

    Laborator13: Adauga rezolvarea primelor 2 exercitii

---
+++ Laborator13/main.py
@@ -0,0 +1,56 @@
+import types
+from functools import reduce
+from math import sqrt
+
+
+### Exercitiul 1 ###
+
+def aop_simulation(before, after, replace=None):
+    def wrap(f):
+        def wrapped_f(*args, **kwargs):
+            if isinstance(before, types.FunctionType):
+                before()  # execute before function
+            if replace and isinstance(replace, types.FunctionType):
+                output = replace(*args, **kwargs)
+            else:
+                output = f(*args, **kwargs)
+            if isinstance(after, types.FunctionType):
+                after()  # execute after function
+            return output
+        return wrapped_f
+    return wrap
+
+
+def is_prime(n: int):
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+
+    for d in range(3, int(sqrt(n)), 2):
+        if n % d == 0:
+            return False
+
+    return True
+
+
+class int(int):
+    @aop_simulation(before=lambda: print('Before'), after=lambda: print('After'), replace=is_prime)
+    def is_prime(self) -> bool:
+        return False
+
+
+### Exercitiul 2 ###
+class str(str):
+    def to_pascal_case(self):
+        split = self.split(" ")
+        mapped = map(lambda s: s[0].upper() + s[1:], split)
+        return reduce(lambda acc, s: acc + s, mapped)
+
+
+if __name__ == "__main__":
+    # Exercitiul 1
+    print(f'3 is prime? {int(3).is_prime()}')
+
+    # Exercitiul 2
+    print(str("Hello world").to_pascal_case())
