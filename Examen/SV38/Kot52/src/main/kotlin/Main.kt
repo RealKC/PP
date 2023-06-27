b33dd822c5cba53b921c8742b3340b2913aba265
Mițca Dumitru <dumitru0mitca@gmail.com>
2023-06-27 20:07:07 +0300

    Exam/SV38: Add the kotlin solution

---
+++ Examen/SV38/Kot52/src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+
+fun main(args: Array<String>) {
+    var count = 0
+
+    (2..100).forEach { i ->
+        (i + 1..100).forEach {j ->
+            (j + 1..100).forEach {
+                count++
+            }
+        }
+    }
+
+    println("The number of subsets of A that contain 1 is $count")
+}
